df4a18fec18d72918a0e0766cc2f0b4d67c2a90a
amrrs <1littlecoder@gmail.com>
2022-01-13 23:55:33 +0530

    spacy streamlit

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import streamlit as st
 import spacy 
-# Importing as module.
-import en_pipeline
+from spacy_streamlit import visualize_parser
 
 st.title("News Bias Words Recognizer")
 
 with st.spinner():
-    nlp = en_pipeline.load()
+    nlp = spacy.load("en_pipeline")
 
 input = st.text_area(label = "Enter your text to get biased words")
 
 doc = nlp(input)
 
-for ent in doc.ents:
-    st.write(ent.text)
+visualize_parser(doc)
+
+
+
+
+
+# doc = nlp(input)
+
+# for ent in doc.ents:
+#     st.write(ent.text)
