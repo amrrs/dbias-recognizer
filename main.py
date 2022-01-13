19db89a6ef2c7b82715b09e5ae4be93f783bae67
amrrs <1littlecoder@gmail.com>
2022-01-13 23:41:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import spacy 
+
+st.title("News Bias Words Recognizer")
+
+with st.spinner():
+    nlp = spacy.load("en_pipeline")
+
+input = st.text_area(label = "Enter your text to get biased words")
+
+doc = nlp(input)
+
+for ent in doc.ents:
+    st.write(ent.text)
