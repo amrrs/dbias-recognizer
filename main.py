7877930ac0c8465e42ea2ec1a5970c0b6c45b9f5
amrrs <1littlecoder@gmail.com>
2022-01-13 23:48:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import spacy 
+# Importing as module.
+import en_pipeline
 
 st.title("News Bias Words Recognizer")
 
 with st.spinner():
-    nlp = spacy.load("en_pipeline")
+    nlp = en_pipeline.load()
 
 input = st.text_area(label = "Enter your text to get biased words")
 
